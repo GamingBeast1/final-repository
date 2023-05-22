5cbd0eb4b99f41c74781acaa87f7d7c231fd8c53
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2023-05-22 17:29:52 +0530

    Create shortener.py

---
+++ shortener.py
@@ -0,0 +1,6 @@
+import requests
+
+def shorten_url(url):
+    # Code to shorten the provided URL using a URL shortening service API
+    # Make HTTP requests to the API and return the shortened URL
+    
