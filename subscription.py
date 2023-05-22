a2ff7a2c56053689e98932a7e88b00e264b719fc
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2023-05-22 17:30:16 +0530

    Create subscription.py

---
+++ subscription.py
@@ -0,0 +1,4 @@
+def check_subscription(update, context):
+    # Code to check if the user is subscribed to the required channel
+    # Send a prompt message or take appropriate actions if not subscribed
+    
